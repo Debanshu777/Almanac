34e0811b47ce4b0e03604735238d865dfc11653a
Debanshu Datta <debanshudatta123@gmail.com>
2021-12-13 18:53:13 +0530

    Added Splash Screen

--- app/src/main/java/com/debanshu777/calendarview/MainActivity.kt
+++ app/src/main/java/com/debanshu777/calendarview/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setTheme(R.style.Theme_CalenderView)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
